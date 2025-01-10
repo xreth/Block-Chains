a9a2b04ee6076dd423bf658a23da995bba75286e
qmze <168580309+xreth@users.noreply.github.com>
2025-01-10 18:15:06 +0000

    Update block-chains.py

--- block-chains.py
+++ block-chains.py
@@ -5,6 +5,7 @@ from typing import List
 
 class Transaction:
     def __init__(self, sender, recipient, amount):
+        
         self.sender = sender
         self.recipient = recipient
         self.amount = amount
